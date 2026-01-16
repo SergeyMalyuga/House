a5239af80fe2ca42304a68f5128bdb0fc57ab775
Sergey Malyuga <sergey@malyuga.ru>
2026-01-16 11:47:02 +0300

    Создать new-collection.component. Адаптировать под мобильные устройства и планшеты.

---
+++ src/app/features/new-collection/new-collection.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-collection',
+  imports: [],
+  templateUrl: './new-collection.component.html',
+  styleUrl: './new-collection.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NewCollectionComponent {
+
+}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeaderComponent } from '../../features/header/header.component';
 import { HeroComponent } from '../../features/hero/hero.component';
 import { ProductsComponent } from '../../features/products/products.component';
+import {NewCollectionComponent} from '../../features/new-collection/new-collection.component';
 
 @Component({
   selector: 'app-main',
-  imports: [HeaderComponent, HeroComponent, ProductsComponent],
+  imports: [HeaderComponent, HeroComponent, ProductsComponent, NewCollectionComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
