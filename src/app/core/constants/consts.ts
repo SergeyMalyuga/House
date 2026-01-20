c86fa658ed25957116813cbd14651f6a411d3389
Sergey Malyuga <sergey@malyuga.ru>
2026-01-20 10:51:59 +0300

    Подключить Angular Material. Создать toggle для переключения отображение списка картин.

--- src/app/core/constants/consts.ts
+++ src/app/core/constants/consts.ts
@@ -2,17 +2,17 @@ export enum AppRoute {
   MAIN = '',
 }
 
-/*export enum CountryRu {
-  FR = 'Франция',
-  DE = 'Германия',
-  GB = 'Англия'
-}*/
-
 export enum CountryEn {
   FR = 'France',
   DE = 'Germany',
   GB = 'England',
 }
+
+export enum DisplayType {
+  LIST = 'list',
+  SLIDER = 'slider',
+}
+
 export const SPACE_INDEX = 2;
 
 export const ACTIVE_COUNTRY_CLASS = 'products__button-active';
--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,17 +1,16 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  computed,
-  signal,
-} from '@angular/core';
-import { CountryEn } from '../../core/constants/consts';
-import { SelectCityDirective } from './directives/select-city.directive';
-import { mockProducts } from './mocks/mock-data';
-import { ProductCardComponent } from '../product-card/product-card.component';
+import {ChangeDetectionStrategy, Component, computed, signal,} from '@angular/core';
+import {CountryEn, DisplayType} from '../../core/constants/consts';
+import {SelectCityDirective} from './directives/select-city.directive';
+import {mockProducts} from './mocks/mock-data';
+import {ProductCardComponent} from '../product-card/product-card.component';
+import {MatButtonToggle, MatButtonToggleGroup} from '@angular/material/button-toggle';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {startWith} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-products',
-  imports: [SelectCityDirective, ProductCardComponent],
+  imports: [SelectCityDirective, ProductCardComponent, MatButtonToggleGroup, MatButtonToggleGroup, MatButtonToggle, ReactiveFormsModule, AsyncPipe],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -19,6 +18,8 @@ import { ProductCardComponent } from '../product-card/product-card.component';
 export class ProductsComponent {
   public currentCity = signal<CountryEn>(CountryEn.FR);
   public readonly City = CountryEn;
+  public displayControl = new FormControl<DisplayType>(DisplayType.LIST);
+  public displayType$ = this.displayControl.valueChanges.pipe(startWith(this.displayControl.value));
 
   public products = computed(() => {
     const city = this.currentCity();
@@ -28,4 +29,10 @@ export class ProductsComponent {
   public onCitySelected(selectedCity: CountryEn): void {
     this.currentCity.set(selectedCity);
   }
+
+  public onToggleSelected(): void {
+    console.log(this.displayControl.value);
+  }
+
+  public readonly DisplayType = DisplayType;
 }
